816a61a8ab87513565c2b0e6cc91997e9eeca225
Gregg Townsend <gmt@cs.arizona.edu>
2014-09-11 10:03:27 -0700

    Comments.

--- vnumber.go
+++ vnumber.go
@@ -8,23 +8,28 @@ import (
 
 type VNumber float64
 
+//  NewNumber -- construct a Goaldi number
 func NewNumber(n float64) *VNumber {
 	vn := VNumber(n)
 	return &vn
 }
 
-func (v *VNumber) String() string {
-	return fmt.Sprintf("%g", float64(*v))
-}
-
+//  VNumber.val -- return underlying float64 value
 func (v *VNumber) val() float64 {
 	return float64(*v)
 }
 
+//  VNumber.String -- convert to Go string
+func (v *VNumber) String() string {
+	return fmt.Sprintf("%g", float64(*v))
+}
+
+//  VNumber.ToString -- convert to Goaldi string
 func (v *VNumber) ToString() *VString {
 	return NewString(v.String())
 }
 
+//  VNumber.Number -- return self
 func (v *VNumber) ToNumber() *VNumber {
 	return v
 }
