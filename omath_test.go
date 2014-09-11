ffe5bbbbaffcc70f2a7fabeb11f5b6c133170d2d
Gregg Townsend <gmt@cs.arizona.edu>
2014-09-11 09:51:22 -0700

    Add string operator (concatenation) test.

--- omath_test.go
+++ omath_test.go
@@ -1,4 +1,4 @@
-//  test_math.go -- test numeric conversions and math operations
+//  omath_test.go -- test numeric conversions and omath.go operations
 
 package goaldi
 
@@ -8,18 +8,19 @@ import (
 )
 
 func TestMath(t *testing.T) {
-	i7, s7 := setup(t, 7)
-	i11, s11 := setup(t, 11)
+	i7, s7 := nspair(t, 7)
+	i11, s11 := nspair(t, 11)
 	t.Log("values:", i7, s7, i11, s11)
-	check(t, "Negate", -7,
+	ck4n(t, "Negate", -7,
 		i7.Negate(), i7.Negate(), s7.Negate(), s7.Negate())
-	check(t, "Add", 18,
+	ck4n(t, "Add", 18,
 		i7.Add(i11), i7.Add(s11), s7.Add(i11), s7.Add(s11))
-	check(t, "Mult", 77,
+	ck4n(t, "Mult", 77,
 		i7.Mult(i11), i7.Mult(s11), s7.Mult(i11), s7.Mult(s11))
 }
 
-func setup(t *testing.T, v float64) (*VNumber, *VString) {
+//  nspair -- return number as a pair (number, string), checking conversions
+func nspair(t *testing.T, v float64) (*VNumber, *VString) {
 	n1 := NewNumber(v)
 	s1 := NewString(fmt.Sprintf("%g", v))
 	n2 := s1.ToNumber()
@@ -33,16 +34,13 @@ func setup(t *testing.T, v float64) (*VNumber, *VString) {
 	return n1, s1
 }
 
-func check(t *testing.T, label string, n0 float64, v1, v2, v3, v4 Value) {
+//  ck4n -- check four numeric values for equality with expected value
+func ck4n(t *testing.T, label string, n0 float64, v1, v2, v3, v4 Value) {
 	t.Log("testing", label)
-	p1 := v1.(*VNumber)
-	p2 := v2.(*VNumber)
-	p3 := v3.(*VNumber)
-	p4 := v4.(*VNumber)
-	n1 := p1.val()
-	n2 := p2.val()
-	n3 := p3.val()
-	n4 := p4.val()
+	n1 := v1.(*VNumber).val()
+	n2 := v2.(*VNumber).val()
+	n3 := v3.(*VNumber).val()
+	n4 := v4.(*VNumber).val()
 	if n0 != n1 || n1 != n2 || n2 != n3 || n3 != n4 {
 		t.Errorf("Expected %g: %g %g %g %g", n0, n1, n2, n3, n4)
 	}
