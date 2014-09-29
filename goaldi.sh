8ba7ce3968932e334512a4fead6dd879cc8d77c5
Gregg Townsend <gmt@cs.arizona.edu>
2014-09-29 15:04:48 -0700

    Make "goaldi -c foo" compile (only) to foo.gir.

--- goaldi.sh
+++ goaldi.sh
@@ -2,28 +2,35 @@
 #
 #  goaldi [options] file [arg...] -- compile and execute Goaldi program
 #
+#  -c	compile only, producing IR on file.gir (interpreter options ignored)
+#
 #  Options -l -t -v -A -J -T are passed along to the interpreter.
 #
 #  Assumes that gtran and gexec are in the search path.
 
-XFLAGS=ltvAJT
-USAGE="usage: $0 [-$XFLAGS] file [arg...]"
+FLAGS=cltvAJT
+USAGE="usage: $0 [-$FLAGS] file [arg...]"
 
 #  process options
 XOPTS=
-while getopts $XFLAGS C; do
+CFLAG=
+while getopts $FLAGS C; do
     case $C in
-	[ltvAJT]) XOPTS="$XOPTS -$C";;
-    ?)
-	echo 1>&2 $USAGE; exit 1;;
+	c)		CFLAG=$C;;
+	[ltvAJT])	XOPTS="$XOPTS -$C";;
+	?)		echo 1>&2 $USAGE; exit 1;;
     esac
 done
 shift $(($OPTIND - 1))
 test $# -lt 1 && echo 1>&2 $USAGE && exit 1
 
 I=$1
+TRAN="gtran preproc $I : yylex : parse : ast2ir : optim -O : json_File : stdout"
 shift
 
 export COEXPSIZE=300000
-exec gtran preproc $I : yylex : parse : ast2ir : optim -O : json_File : stdout |
-  gexec $XOPTS "$@"
+if [ -n "$CFLAG" ]; then
+    exec $TRAN >${I%.*}.gir
+else
+    exec $TRAN | gexec $XOPTS "$@"
+fi
