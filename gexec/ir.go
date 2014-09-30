0a1cf16227daf00930ff961cf41b2571fe8ef8c8
Gregg Townsend <gmt@cs.arizona.edu>
2014-09-30 13:21:32 -0700

    Add print formats for some nested structs.

--- gexec/ir.go
+++ gexec/ir.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -66,19 +67,23 @@ type ir_Invocable struct {
 	NameList []string
 	All      interface{}
 }
+
 type ir_Link struct {
 	Coord    *ir_coordinate
 	NameList []string
 }
+
 type ir_Record struct {
 	Coord     *ir_coordinate
 	Name      string
 	FieldList []string
 }
+
 type ir_Global struct {
 	Coord    *ir_coordinate
 	NameList []string
 }
+
 type ir_Function struct {
 	Coord      *ir_coordinate
 	Name       string
@@ -90,90 +95,129 @@ type ir_Function struct {
 	CodeStart  *ir_Label
 	Lvalset    []string
 }
+
 type ir_chunk struct {
 	Label    *ir_Label
 	InsnList []interface{} // heterogeneous
 }
+
 type ir_Tmp struct {
 	Name string
 }
+
+func (i ir_Tmp) String() string {
+	return fmt.Sprintf("%s", i.Name)
+}
+
 type ir_TmpLabel struct {
 	Name string
 }
+
+func (i ir_TmpLabel) String() string {
+	return fmt.Sprintf("%s", i.Name)
+}
+
 type ir_TmpClosure struct {
 	Name string
 }
+
+func (i ir_TmpClosure) String() string {
+	return fmt.Sprintf("%s", i.Name)
+}
+
 type ir_Label struct {
 	Value string
 }
+
+func (i ir_Label) String() string {
+	return fmt.Sprintf("%s", i.Value)
+}
+
 type ir_Var struct {
 	Coord *ir_coordinate
 	Lhs   *ir_Tmp
 	Name  string
 }
+
 type ir_Key struct {
 	Coord     *ir_coordinate
 	Lhs       *ir_Tmp
 	Name      string
 	FailLabel *ir_Label
 }
+
 type ir_IntLit struct {
 	Coord *ir_coordinate
 	Lhs   *ir_Tmp
 	Val   string
 }
+
 type ir_RealLit struct {
 	Coord *ir_coordinate
 	Lhs   *ir_Tmp
 	Val   string
 }
+
 type ir_StrLit struct {
 	Coord *ir_coordinate
 	Lhs   *ir_Tmp
 	Len   string
 	Val   string
 }
+
 type ir_CsetLit struct {
 	Coord *ir_coordinate
 	Lhs   *ir_Tmp
 	Len   string
 	Val   string
 }
+
 type ir_operator struct {
 	Name  string
 	Arity string
 	Rval  interface{}
 }
+
+func (i ir_operator) String() string {
+	return fmt.Sprintf("%s%s %v", i.Arity, i.Name, i.Rval)
+}
+
 type ir_MakeClosure struct {
 	Coord *ir_coordinate
 	Lhs   interface{}
 	Name  string
 }
+
 type ir_Move struct {
 	Coord *ir_coordinate
 	Lhs   *ir_Tmp
 	Rhs   *ir_Tmp
 }
+
 type ir_MoveLabel struct {
 	Coord *ir_coordinate
 	Lhs   *ir_TmpLabel
 	Label *ir_Label
 }
+
 type ir_Deref struct {
 	Coord *ir_coordinate
 	Lhs   *ir_Tmp
 	Value *ir_Tmp
 }
+
 type ir_Assign struct {
 	Coord  *ir_coordinate
 	Target *ir_Tmp
 	Value  *ir_Tmp
 }
+
 type ir_MakeList struct {
 	Coord     *ir_coordinate
 	Lhs       *ir_Tmp
 	ValueList []interface{} // heterogeneous
 }
+
 type ir_Field struct {
 	Coord     *ir_coordinate
 	Lhs       *ir_Tmp
@@ -181,6 +225,7 @@ type ir_Field struct {
 	Field     string
 	FailLabel *ir_Label
 }
+
 type ir_OpFunction struct {
 	Coord      *ir_coordinate
 	Lhs        *ir_Tmp
@@ -189,6 +234,7 @@ type ir_OpFunction struct {
 	ArgList    []interface{} // heterogeneous
 	FailLabel  *ir_Label
 }
+
 type ir_Call struct {
 	Coord      *ir_coordinate
 	Lhs        *ir_Tmp
@@ -197,56 +243,72 @@ type ir_Call struct {
 	ArgList    []interface{} // heterogeneous
 	FailLabel  *ir_Label
 }
+
 type ir_ResumeValue struct {
 	Coord      *ir_coordinate
 	Lhs        *ir_Tmp
-	Lhsclosure interface{} // ir_Tmp or ir_TmpClosure
-	Closure    interface{} // ir_Label or ir_TmpClosure
+	Lhsclosure *ir_TmpClosure //#%#% ir_Tmp also seen?
+	Closure    *ir_TmpClosure //#%#% ir_Label also seen?
 	FailLabel  *ir_Label
 }
+
 type ir_EnterInit struct {
 	Coord      *ir_coordinate
 	StartLabel *ir_Label
 }
+
 type ir_Goto struct {
 	Coord       *ir_coordinate
 	TargetLabel *ir_Label
 }
+
 type ir_IndirectGoto struct {
 	Coord          *ir_coordinate
 	TargetTmpLabel *ir_TmpLabel
 }
+
 type ir_Succeed struct {
 	Coord       *ir_coordinate
 	Expr        *ir_Tmp
 	ResumeLabel *ir_Label
 }
+
 type ir_Fail struct {
 	Coord *ir_coordinate
 }
+
 type ir_Create struct {
 	Coord      *ir_coordinate
 	Lhs        *ir_Tmp
 	CoexpLabel *ir_Label
 }
+
 type ir_CoRet struct {
 	Coord       *ir_coordinate
 	Value       *ir_Tmp
 	ResumeLabel interface{}
 }
+
 type ir_CoFail struct {
 	Coord *ir_coordinate
 }
+
 type ir_ScanSwap struct {
 	Coord   *ir_coordinate
 	Subject *ir_Tmp
 	Pos     *ir_Tmp
 }
+
 type ir_Unreachable struct {
 	Coord *ir_coordinate
 }
+
 type ir_coordinate struct {
 	File   string
 	Line   string
 	Column string
+}
+
+func (i ir_coordinate) String() string {
+	return fmt.Sprintf("%s:%s:%s", i.File, i.Line, i.Column)
 }
