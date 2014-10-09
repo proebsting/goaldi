a2f9c02f65e9828a81df2ab3c2d899ceaf029d5f
Gregg Townsend <gmt@cs.arizona.edu>
2014-10-09 12:02:00 -0700

    Register stdlib functions dynamically at init time.

--- stdlib.go
+++ stdlib.go
@@ -12,32 +12,42 @@ import (
 	"strings"
 )
 
-var StdLib = []*VProcedure{
+//  StdLib is the set of procedures available at link time
+var StdLib = make(map[string]*VProcedure)
 
-	GoProcedure("abs", math.Abs),
-	GoProcedure("min", math.Min), // not like Icon: only 2 args
-	GoProcedure("max", math.Max), // not like Icon: only 2 args
-	GoProcedure("log", math.Log), // not like Icon: no 2nd arg
-	GoProcedure("sqrt", math.Sqrt),
+//  LibProc registers a Go function as a standard library procedure.
+//  This must be done before linking (e.g. via init func) to be effective.
+func LibProc(name string, f interface{}) {
+	StdLib[name] = GoProcedure(name, f)
+}
 
-	GoProcedure("intn", rand.Intn),
-	GoProcedure("seed", rand.Seed),
+//  This init function adds a set of Go functions to the standard library
+func init() {
 
-	GoProcedure("equalfold", strings.EqualFold),
-	GoProcedure("replace", strings.Replace),
-	GoProcedure("toupper", strings.ToUpper),
-	GoProcedure("tolower", strings.ToLower),
-	GoProcedure("trim", strings.Trim),
+	LibProc("abs", math.Abs)
+	LibProc("min", math.Min) // not like Icon: only 2 args
+	LibProc("max", math.Max) // not like Icon: only 2 args
+	LibProc("log", math.Log) // not like Icon: no 2nd arg
+	LibProc("sqrt", math.Sqrt)
 
-	GoProcedure("print", fmt.Print),
-	GoProcedure("println", fmt.Println),
-	GoProcedure("printf", fmt.Printf),
-	GoProcedure("fprint", fmt.Fprint),
-	GoProcedure("fprintln", fmt.Fprintln),
-	GoProcedure("fprintf", fmt.Fprintf),
-	GoProcedure("write", fmt.Println), // not like Icon: no file, spacing
-	GoProcedure("writes", fmt.Print),  // not like Icon: no file, spacing
+	LibProc("intn", rand.Intn)
+	LibProc("seed", rand.Seed)
 
-	GoProcedure("exit", os.Exit),
-	GoProcedure("remove", os.Remove),
+	LibProc("equalfold", strings.EqualFold)
+	LibProc("replace", strings.Replace)
+	LibProc("toupper", strings.ToUpper)
+	LibProc("tolower", strings.ToLower)
+	LibProc("trim", strings.Trim)
+
+	LibProc("print", fmt.Print)
+	LibProc("println", fmt.Println)
+	LibProc("printf", fmt.Printf)
+	LibProc("fprint", fmt.Fprint)
+	LibProc("fprintln", fmt.Fprintln)
+	LibProc("fprintf", fmt.Fprintf)
+	LibProc("write", fmt.Println) // not like Icon: no file, spacing
+	LibProc("writes", fmt.Print)  // not like Icon: no file, spacing
+
+	LibProc("exit", os.Exit)
+	LibProc("remove", os.Remove)
 }
