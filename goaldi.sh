8a679f0451ebb5fdddc779806d31ffb333e7665b
Gregg Townsend <gmt@cs.arizona.edu>
2014-12-18 13:36:03 -0700

    Add "goaldi -d" option to produce Dot file.

--- goaldi.sh
+++ goaldi.sh
@@ -3,23 +3,26 @@
 #  goaldi [options] file [arg...] -- compile and execute Goaldi program
 #
 #  -c	compile only, producing IR on file.gir (interpreter options ignored)
+#  -d	compile only, producing Dot directives on file.dot
 #  -N	no optimization
 #
 #  Options -l -t -v -A -D -F -J -P -T are passed along to the interpreter.
 #
 #  Assumes that gtran and gexec are in the search path.
 
-FLAGS=cNltvADFJPT
+FLAGS=cdNltvADFJPT
 USAGE="usage: $0 [-$FLAGS] file [arg...]"
 TMP=/tmp/gdi.$$.gir
 
 #  process options
 XOPTS=
 CFLAG=
+DFLAG=
 OPT=": optim -O"
 while getopts $FLAGS C; do
     case $C in
 	c)			CFLAG=$C;;
+	d)			DFLAG=$C;;
 	N)			OPT="";;
 	[ltvADFJPT])	XOPTS="$XOPTS -$C";;
 	?)			echo 1>&2 $USAGE; exit 1;;
@@ -29,13 +32,20 @@ shift $(($OPTIND - 1))
 test $# -lt 1 && echo 1>&2 $USAGE && exit 1
 
 I=$1
+DOT="gtran preproc $I : yylex : parse : ast2ir $OPT : dot_File : stdout"
 TRAN="gtran preproc $I : yylex : parse : ast2ir $OPT : json_File : stdout"
 shift
 
 export COEXPSIZE=300000
-if [ -n "$CFLAG" ]; then
+
+if [ -n "$DFLAG" ]; then	# -d: produce file.dot, and quit
+    exec $DOT >${I%.*}.dot
+fi
+
+if [ -n "$CFLAG" ]; then	# -c: produce file.gir, and quit
     exec $TRAN >${I%.*}.gir
 fi
 
+#  translate and execute
 trap 'rm -f $TMP; exit' 0 1 2 15
 $TRAN >$TMP && exec gexec $XOPTS $TMP "$@"
