e64a2b1be29ff91ac3a3dc5d08b26f1f6ffa89a9
Gregg Townsend <gmt@cs.arizona.edu>
2015-02-04 13:20:35 -0700

    Change "dynamic" to "with" in test.

--- gtests/dynamic.gd
+++ gtests/dynamic.gd
@@ -6,26 +6,35 @@ global i
 global j
 
 procedure main() {
-	show("a"); dynamic %x := 100
-	show("b")
-	every i := !2 do {
-		show("   c");  %x +:= 1
-		show("   d");  dynamic %y := 200
-		show("   e");  %y +:= 2
-		show("   f")
-		showxyz("   g", %x, %y, "--")
-		every j := !3 do {
-			show("      h");  dynamic %x := 300
-			show("      i");  %x +:= 3
-			show("      j");  %y +:= 4
-			show("      k");  dynamic %z := 400
-			show("      l")
-			showxyz("      m", %x, %y, %z)
+	show("a")
+	with %x := 100 do {
+		show("b")
+		every i := !2 do {
+			show("   c");  %x +:= 1
+			show("   d")
+			with %y := 200 do {
+				show("   e");  %y +:= 2
+				show("   f")
+				showxyz("   g", %x, %y, "--")
+				every j := !3 do {
+					show("      h")
+					with %x := 300 do {
+						show("      i");  %x +:= 3
+						show("      j");  %y +:= 4
+						show("      k")
+						with %z := 400 do {
+							show("      l")
+							showxyz("      m", %x, %y, %z)
+						}
+					}
+				}
+				with %z := 500 do {
+					show("   u")
+				}
+			}
 		}
-		dynamic %z := 500
-		show("   u")
-	}	
 	show("v")
+	}
 }
 
 procedure show(label) {
@@ -53,5 +62,5 @@ procedure zval() {
 
 procedure nope(e) {
 	# extract variable name from exception message
-	return "(" || string(e)[-3:0]	
+	return "(" || string(e)[-3:0]
 }
