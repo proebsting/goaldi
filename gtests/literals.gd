998298fe290a9da6657e292c6179c699e9890709
Gregg Townsend <gmt@cs.arizona.edu>
2015-02-11 12:21:00 -0700

    Fix string oct/hex/uni escapes near EOL.

--- gtests/literals.gd
+++ gtests/literals.gd
@@ -5,16 +5,16 @@
 procedure main() {
 
 	# should interpret excapes in quoted string
-	^s := "\b\d\e\f\l\n\r\t\v\'\"\\"
+	^s := "\b\d\e\f\l\n\r\t\v\'\"\\\067\130\x58\u0058\^H"
 	write(image(s))
 	every ^c := !s do
 		write(ord(c), " ", image(c))
 
-	# this gets error in both Goaldi and Jcon
-	# write(image("\a\c\g\h\i\j\k\m\o\p\q\s\u\w\x\u\z"))
+	# these escapes should have no effect
+	write(image("\a\c\g\h\i\j\k\m\o\p\q\s\u\w\x\y\z"))
 
 	# should not interpret excapes in raw-quoted string
-	s := `\b\d\e\f\l\n\r\t\v\'\"\\`
+	s := `\b\d\e\f\l\n\r\t\v\'\"\\\067\130\x58\u0058\^H`
 	write(*s, " ", image(s))
 	write(*s, " ", s)
 
