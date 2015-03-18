cb8d6e0160bac4f20cfc6f2cbb49e0220d514e56
Gregg Townsend <gmt@cs.arizona.edu>
2015-03-18 06:59:09 -0700

    Hook ntran parts together in a pipeline.

--- ntran/gtran.gd
+++ ntran/gtran.gd
@@ -6,7 +6,15 @@ procedure main(args[]) {
 		^fbase := if fname[-3:0]==".gd" then fname[1:-3] else fname
 		^pipeline := create !open(fbase || ".gd")
 		pipeline := create lex(pipeline, fbase || ".gd")
-		pipeline := create tee(pipeline, fbase || ".tk")
+		pipeline := create tee(pipeline, fbase || ".tok")
+		pipeline := create parse(pipeline)
+		pipeline := create tee(pipeline, fbase || ".ast")
+		pipeline := create ast2ir(pipeline)
+		pipeline := create tee(pipeline, fbase || ".rir")
+		pipeline := create optim(pipeline, ["-O"])
+		pipeline := create tee(pipeline, fbase || ".oir")
+		pipeline := create json_File(pipeline)
+		pipeline := create tee(pipeline, fbase || ".gir")
 		# pipeline := create stdout(pipeline)
 		pipeline := create sink(pipeline)
 		@pipeline	# wait for processes to finish and close
@@ -17,7 +25,11 @@ procedure main(args[]) {
 procedure tee(src, fname) {
 	^f := open(fname, "w")
 	while ^v := @src do {
-		f.write(v.image())
+		if type(v) === string then {
+			f.write(v)
+		} else {
+			f.write(v.image())
+		}
 		suspend v
 	}
 	f.close()
