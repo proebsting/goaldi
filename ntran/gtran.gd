0e76d022b6595a010961a869c15f0226ca998358
Gregg Townsend <gmt@cs.arizona.edu>
2015-03-23 09:10:53 -0700

    Rework ntran test scaffolding.

--- ntran/gtran.gd
+++ ntran/gtran.gd
@@ -6,16 +6,11 @@ procedure main(args[]) {
 		^fbase := if fname[-3:0]==".gd" then fname[1:-3] else fname
 		^pipeline := create !open(fbase || ".gd")
 		pipeline := create lex(pipeline, fbase || ".gd")
-		pipeline := create tee(pipeline, fbase || ".tok")
 		pipeline := create parse(pipeline)
-		pipeline := create tee(pipeline, fbase || ".ast")
 		pipeline := create ast2ir(pipeline)
-		pipeline := create tee(pipeline, fbase || ".rir")
 		pipeline := create optim(pipeline, ["-O"])
-		pipeline := create tee(pipeline, fbase || ".oir")
 		pipeline := create json_File(pipeline)
-		pipeline := create tee(pipeline, fbase || ".gir")
-		# pipeline := create stdout(pipeline)
+		pipeline := create tee(pipeline, fbase || ".json")
 		pipeline := create sink(pipeline)
 		@pipeline	# wait for processes to finish and close
 	}
@@ -35,7 +30,7 @@ procedure tee(src, fname) {
 	f.close()
 }
 
-#  (terminal) pipeline component to write stream stdout
+#  (terminal) pipeline component to write stream to stdout
 procedure stdout(src) {
 	while write(image(@src))
 }
