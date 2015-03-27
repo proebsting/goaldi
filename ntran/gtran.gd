6536a399af39b13b6fa92ae218fc63cbd40f4dc0
Gregg Townsend <gmt@cs.arizona.edu>
2015-03-27 08:38:23 -0700

    Give ntran a simple options processor.

--- ntran/gtran.gd
+++ ntran/gtran.gd
@@ -2,17 +2,14 @@
 #  (quick-and-dirty experimental version)
 
 procedure main(args[]) {
-	local nflag
-	if args[1] == "-N" then {
-		nflag := @args
-	}
+	local opts := options(args, "N")
 	every ^fname := !args do {
 		^fbase := if fname[-3:0]==".gd" then fname[1:-3] else fname
 		^pipeline := create !open(fbase || ".gd")
 		pipeline := create lex(pipeline, fbase || ".gd")
 		pipeline := create parse(pipeline)
 		pipeline := create ast2ir(pipeline)
-		if /nflag then {
+		if /opts["N"] then {
 			pipeline := create optim(pipeline, ["-O"])
 		}
 		pipeline := create json_File(pipeline)
@@ -43,4 +40,30 @@ procedure stdout(src) {
 #  (terminal) pipeline component to toss everything into a black hole
 procedure sink(src) {
 	while @src
+}
+
+#  options(args,optstring) -- simplified command option processing
+#
+#  Interprets any char of optstring as an allowable flag argument.
+#  Does not allow value arguments.
+#  Removes option arguments from args and returns a table of flags.
+#  Aborts on error.
+
+procedure options(args, optstring) {
+	every (^allowed := set()).insert(!optstring)
+	^seen := table()
+	while args[1][1] == "-" do {
+		^flags := args.get()[2:0]
+		if flags == "-" then {
+			break	# exit on "--"
+		}
+		every ^c := !flags do {
+			if allowed[c] then {
+				seen[c] := c
+			} else {
+				stop("unrecognized option: -", c)
+			}
+		}
+	}
+	return seen
 }
