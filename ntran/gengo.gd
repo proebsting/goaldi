81d833475721284bc60726af8bccaa5de2f52233
Todd Proebsting <proebsting@gmail.com>
2015-03-28 21:49:07 -0700

    starting the Go translation skeleton

--- ntran/gengo.gd
+++ ntran/gengo.gd
@@ -4,13 +4,7 @@ procedure go_File(f, irgen) {
 	local init
 	while local p := @irgen do {
 		/init := go_start(f, p)
-		case type(p) of {
-			default:		throw("unrecognized type", p)
-			ir_Global:		suspend go_global(f, p)
-			ir_Record:		suspend go_record(f, p)
-			ir_Initial:		suspend go_initial(f, p)
-			ir_Function:	suspend go_function(f, p)
-		}
+		p.go(f)
 	}
 }
 
@@ -21,20 +15,48 @@ procedure go_start(f, p) {
 	return namespace
 }
 
-procedure go_global(f, p) {
-	f.write(`var `, p.name, ` = g.NewVariable(g.NilValue)`)
+procedure ir_Global.go(f) {
+	f.write(`var `, self.name, ` = g.NewVariable(g.NilValue)`)
 }
 
-procedure go_record(f, p) {
+procedure ir_Record.go(f) {
 	^flist := "[]string{"
 	^parent := "nil"
-	every flist ||:= image(!p.fieldList) || ","
-	f.write(`var `, p.name, ` = g.NewCtor("`,
-		p.name, `", `, parent, `, `, flist, `})`)
+	every flist ||:= image(!self.fieldList) || ","
+	f.write(`var `, self.name, ` = g.NewCtor("`,
+		self.name, `", `, parent, `, `, flist, `})`)
 }
 
-procedure go_initial(f, p) {
+procedure ir_Initial.go(f) {
+	throw(self)
 }
 
-procedure go_function(f, p) {
+procedure ir_Function.go(f) {
+	every ^c := !self.codeList do {
+		c.go(f)
+	}
+}
+
+procedure ir_chunk.go(f) {
+	every ^c := !self.insnList do {
+		c.go(f)
+	}
+}
+
+procedure ir_Fail.go(f) {
+}
+
+procedure ir_Call.go(f) {
+}
+
+procedure ir_OpFunction.go(f) {
+}
+
+procedure ir_Var.go(f) {
+}
+
+procedure ir_RealLit.go(f) {
+}
+
+procedure ir_EnterScope.go(f) {
 }
