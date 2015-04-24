c3f65f11d4203be6f22eedcde9e4ab328a8863dd
Gregg Townsend <gmt@cs.arizona.edu>
2015-04-24 07:21:59 -0700

    Reorganize and renumber runerrs test.

--- tests/runerrs.gd
+++ tests/runerrs.gd
@@ -20,84 +20,102 @@ procedure try(i) {
 procedure provoke(i) {
 	case i of {
 
+		# variables
 		10: %huh
 
-		20: "x" || main
-		21: 1 + "x"
-		22: 1 + main
-		23: ?(-3)
-		24: select { 666 @: 0 : 0}
+		# exceptions
+		21: throw("my double error", 12, 34)
+		22: throw("my own error")
+		23: throw("my nil error", nil)
+		24: throw("my pi error", %pi)
+		25: throw(99, %phi)
 
-		32: ?"abcd" := 5
-		33: @"efgh"
-		34: (^s := "lmno") @: [666]
+		# z : nil
 
-		40: constructor()
-		41: constructor(%pi)
-		42: constructor("r", "a", "2", "c")
-		43: tuple(1,2,3)
-		44: tuple(a:1,b:2,a:3)
+		# t : type
 
-		49: 10 to []
-		50: nil to 10
-		51: "x" to 10
-		52: 1 to nil
-		53: 1 to []
-		54: 1 to 10 by 0
+		# n : number
+		101: 1 + "x"
+		102: 1 + main
+		103: ?(-3)
+		104: 3 @: 7
+		105: 3(1,2,e:5)
 
-		57: 57 ||| []
-		58: [] ||| 58
+		121: 10 to []
+		122: nil to 10
+		123: "x" to 10
+		124: 1 to nil
+		125: 1 to []
+		126: 1 to 10 by 0
 
-		60: channel()[3]
-		61: 3 @: 7
-		62: [].sort(-1)
-		63: r().huh
-		64: [].huh
+		140: gmean(0,1,2)
+		141: gmean(3,1,-1)
+		142: hmean(-1,3)
+		143: hmean(2,0)
 
+		# s : string
+		201: "x" || main
+		210: ?"abcd" := 5
+		221: @"efgh"
+		222: (^s := "lmno") @: [666]
 
-		69: echolist(a:12)
-		70: echo(1, a:2)
-		71: echo(b:3, b:4)
-		72: echo(d:5)
-		73: r().echo(e:5)
-		74: r().echo(self:6)
-		75: r(x:7)
-		76: r(1,2,3,4)
-		77: r.d
-		78: printf(s:"hello")
-		79: 3(1,2,e:5)
+		240: char(-1)
+		241: char(123456x)
+		242: ord("ab")
+		243: left("ab",5,"")
+		244: center("x",7,"")
+		245: right("xyz",26,"")
+		246: map("abc", "123", "3210")
 
-		80: file("x", "q")
-		81: file("/no/such/file")
-		82: file("/bin", "w")
-		83: file("/bin/ls", "w")
-		84: file("/dev/null").write()
-		85: !file("/dev/null", "w")
-		86: file("/dev/null").close().read()
-		87: file("/dev/null").close().close()
+		# f : file
+		301: file("x", "q")
+		302: file("/no/such/file")
+		303: file("/bin", "w")
+		304: file("/bin/ls", "w")
+		305: file("/dev/null").write()
+		306: !file("/dev/null", "w")
+		311: file("/dev/null").close().read()
+		312: file("/dev/null").close().close()
+		315: remove("/no/such/file")
 
-		120: gmean(0,1,2)
-		121: gmean(3,1,-1)
-		122: hmean(-1,3)
-		123: hmean(2,0)
+		# c : channel
+		341: select { 666 @: 0 : 0}
 
-		130: char(-1)
-		131: char(123456x)
-		132: ord("ab")
-		133: left("ab",5,"")
-		134: center("x",7,"")
-		135: right("xyz",26,"")
-		136: map("abc", "123", "3210")
+		# m : methodvalue
 
-		140: remove("/no/such/file")
+		# p : procedure
+		361: echolist(a:12)
+		362: echo(1, a:2)
+		363: echo(b:3, b:4)
+		364: echo(d:5)
+		365: printf(s:"hello")
 
-		150: regex("(")
+		# L : list
+		401: 57 ||| []
+		402: [] ||| 58
+		403: channel()[3]
+		404: [].sort(-1)
 
-		195: throw("my double error", 12, 34)
-		196: throw("my own error")
-		197: throw("my nil error", nil)
-		198: throw("my pi error", %pi)
-		199: throw(99, %phi)
+		# S : set
+
+		# T : table
+
+		# R : record
+		501: r.d
+		502: r().huh
+		503: [].huh
+		511: r().echo(e:5)
+		512: r().echo(self:6)
+		521: r(x:7)
+		522: r(1,2,3,4)
+		541: constructor()
+		542: constructor(%pi)
+		543: constructor("r", "a", "2", "c")
+		551: tuple(1,2,3)
+		552: tuple(a:1,b:2,a:3)
+
+		# X : external
+		901: regex("(")
 
 		default: return fail
 	}
