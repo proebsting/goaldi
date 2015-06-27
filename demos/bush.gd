a2da93fa8985a6cd92eecaf97e39ec76eb134642
Gregg Townsend <gmt@cs.arizona.edu>
2015-06-27 07:42:20 -0700

    Twiddle the bush demo.

--- demos/bush.gd
+++ demos/bush.gd
@@ -1,17 +1,22 @@
 #  an early demo of Goaldi turtle graphics
 #  draws a multi-colored bush; redraws on a click or touch
 
+global clist := ["black", "brown", "green", "blue", "gray", "purple"]
+global dlist := ["black", "brown", "red", "orange", "gold", "gray"]
+
 procedure main() {
 	randomize()
 	^w := canvas()
 	repeat {
-		bush(w)
+		moon(w)
+		branch(w, 3, 8)
 		while (@w.Events).Action ~= 2
 		w.Reset()
+		clist :=: dlist
 	}
 }
 
-procedure bush(w) {
+procedure moon(w) {
 	w.Reset()
 	w.color("white")
 	w.Forward(-120)
@@ -24,18 +29,17 @@ procedure bush(w) {
 		sleep(0.001)
 	}
 	w.turn(90)
-	branch(w, 3, 8)
 }
 
 procedure branch(w, z, n) {
-	/static clist := ["black", "brown", "red", "orange", "gold", "gray"]
 	w.color(?clist)
 	w.Size := z
-	w.turn(?90 - 45)
 	w.Forward(8 + ?17)
 	sleep(0.002)
 	if n > 0 then {
+		w.turn(?60 - 30)
 		branch(w.copy(), .93 * z, n-1)
+		w.turn(?60 - 30)
 		branch(w.copy(), .87 * z, n-1)
 	}
 }
